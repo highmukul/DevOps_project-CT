33b5659f490e5b01ddeff55dc54a821ad96449f8
Mukul Kumar Goel <166716851+high-mukul@users.noreply.github.com>
2024-07-16 15:37:59 +0530

    Initial commmit

---
+++ src/main/java/com/highmukul/CTproject/CTprojectApplication.java
@@ -0,0 +1,13 @@
+package com.highmukul.CTproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CTprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CTprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/highmukul/CTproject/CTprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highmukul.CTproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CTprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
